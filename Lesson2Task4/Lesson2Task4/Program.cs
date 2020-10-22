6a8499364bcb8f48ab18a3f0fe51d53a27b15968
unknown <rostykvoznyak@gmail.com>
2020-10-22 23:22:45 +0300

    Lesson2Task4

---
+++ Lesson2Task4/Lesson2Task4/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Lesson2Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const long year = 1993;
+            Console.WriteLine($"Birth year:{year}");
+        }
+    }
+}
